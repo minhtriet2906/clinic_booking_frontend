26720335fa145222a02a0577c04bc3f1cb85e571
minhtriet2906 <swaggiehippdude@gmail.com>
2023-04-10 15:07:40 +0700

    update user login

---
+++ src/containers/Auth/Login.js
@@ -0,0 +1,120 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push } from "connected-react-router";
+
+import * as actions from "../../store/actions";
+import './Login.scss';
+import { FormattedMessage } from 'react-intl';
+import { handleLogin } from '../../services/userService';
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: '',
+            isShowPassword: false,
+            errorMessage: ''
+        }
+    }
+
+    handleOnChangeUsername = (event) => {
+        this.setState({
+            username: event.target.value,
+        })
+    }
+
+    handleOnChangePassword = (event) => {
+        this.setState({
+            password: event.target.value,
+        })
+    }
+
+    handleLogin = async () => {
+        this.setState({
+            errorMessage: ''
+        })
+
+        try {
+            let data = await handleLogin(this.state.username, this.state.password);
+
+            if (data && data.errorCode !== 0) {
+                this.setState({
+                    errorMessage: data.message
+                })
+            }
+
+            if (data && data.errorCode === 0) {
+                this.props.userLoginSuccess(data.user);
+                console.log('Login Success');
+            }
+
+        } catch (error) {
+            if (error.response) {
+                if (error.response.data) {
+                    this.setState({
+                        errorMessage: error.response.data.message
+                    })
+                }
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div className='login-background'>
+                <div className='login-container'>
+                    <div className='login-content row'>
+                        <div className='col-12 text-login' >Login</div>
+                        <div className='col-12 form group login-input'>
+                            <label>Username: </label>
+                            <input type='text'
+                                placeholder='Enter your username'
+                                className='form-control'
+                                value={this.state.username}
+                                onChange={(event) => this.handleOnChangeUsername(event)} />
+                        </div>
+                        <div className='col-12 form group'>
+                            <label>Password: </label>
+                            <input type='password'
+                                placeholder='Enter your password'
+                                className='form-control'
+                                onChange={(event) => this.handleOnChangePassword(event)} />
+                        </div>
+                        <div className='col-12' style={{ color: 'red' }}>
+                            {this.state.errorMessage}
+                        </div>
+                        <div className='col-12'>
+                            <button className='btn-login' onClick={() => this.handleLogin()}>Login</button>
+                        </div>
+                        <div className='col-12'>
+                            <span className='forgot-password'>Forget your password ?</span>
+                        </div>
+                        <div className='col-12 text-center mt-3'>
+                            <span className='text-other-login'>Or Login with</span>
+                        </div>
+                        <div className='col-12 social-login'>
+                            <i className="fab fa-google google"></i>
+                            <i className="fab fa-facebook facebook"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        lang: state.app.language
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        navigate: (path) => dispatch(push(path)),
+        userLoginSuccess: (userInfor) => dispatch(actions.userLoginSuccess(userInfor))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
