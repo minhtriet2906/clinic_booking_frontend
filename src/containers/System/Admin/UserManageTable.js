382a74125f815a688ec44557243b68795a41ee33
minhtriet2906 <swaggiehippdude@gmail.com>
2023-05-04 11:18:34 +0700

    create markdown section

--- src/containers/System/Admin/UserManageTable.js
+++ src/containers/System/Admin/UserManageTable.js
@@ -3,6 +3,21 @@ import { FormattedMessage } from 'react-intl';
 import * as actions from "../../../store/actions"
 import { connect } from 'react-redux';
 import './UserManageTable.scss'
+import MarkdownIt from 'markdown-it';
+import MdEditor from 'react-markdown-editor-lite';
+// import style manually
+import 'react-markdown-editor-lite/lib/index.css';
+
+// Register plugins if required
+// MdEditor.use(YOUR_PLUGINS_HERE);
+
+// Initialize a markdown parser
+const mdParser = new MarkdownIt(/* Markdown-it options */);
+
+// Finish!
+function handleEditorChange({ html, text }) {
+    console.log('handleEditorChange', html, text);
+}
 
 class UserManageTable extends Component {
 
@@ -38,36 +53,41 @@ class UserManageTable extends Component {
     render() {
         let arrUsers = this.state.users;
         return (
-            <table id='user-manage-table'>
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Email</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Address</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {arrUsers && arrUsers.length > 0 && arrUsers.map((item, index) => {
-                        return (
-                            <tr key={index}>
-                                <td>{item.id}</td>
-                                <td>{item.email}</td>
-                                <td>{item.firstName}</td>
-                                <td>{item.lastName}</td>
-                                <td>{item.address}</td>
-                                <td>
-                                    <button className='btn-edit' onClick={() => this.handleEditUser(item)}>Edit</button>
-                                    <button className='btn-delete' onClick={() => this.handleDeleteUser(item)}>Delete</button>
-                                </td>
-                            </tr>
-                        )
-                    }
-                    )}
-                </tbody>
-            </table>
+            <React.Fragment>
+
+
+                <table id='user-manage-table'>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Email</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                            <th>Address</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {arrUsers && arrUsers.length > 0 && arrUsers.map((item, index) => {
+                            return (
+                                <tr key={index}>
+                                    <td>{item.id}</td>
+                                    <td>{item.email}</td>
+                                    <td>{item.firstName}</td>
+                                    <td>{item.lastName}</td>
+                                    <td>{item.address}</td>
+                                    <td>
+                                        <button className='btn-edit' onClick={() => this.handleEditUser(item)}>Edit</button>
+                                        <button className='btn-delete' onClick={() => this.handleDeleteUser(item)}>Delete</button>
+                                    </td>
+                                </tr>
+                            )
+                        }
+                        )}
+                    </tbody>
+                </table>
+                <MdEditor style={{ height: '500px' }} renderHTML={text => mdParser.render(text)} onChange={handleEditorChange} />
+            </React.Fragment>
         );
     }
 
