4946d6d7a12dd45a94cb3caf10a8154ff54abb43
minhtriet2906 <swaggiehippdude@gmail.com>
2023-05-19 16:17:23 +0700

    create booking modal component

---
+++ src/containers/Patient/Doctor/Modal/BookingModal.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+import './BookingModal.scss'
+import { FormattedMessage } from 'react-intl';
+import { Modal } from 'reactstrap';
+import DoctorProfile from '../DoctorProfile';
+
+class BookingModal extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    async componentDidMount() {
+
+    }
+
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+
+    }
+
+
+    render() {
+        let { isOpenBookingModal, closeBookingModal, bookingTime } = this.props;
+        return (
+            <Modal isOpen={isOpenBookingModal}
+                className='booking-modal-container'
+                size='lg'
+                centered>
+                <div className='booking-modal-content'>
+                    <div className='booking-modal-header'>
+                        <span className='left'>Booking Info</span>
+                        <span className='right'
+                            onClick={closeBookingModal}>
+                            <i className='fas fa-times'></i>
+                        </span>
+                    </div>
+                    <div className='booking-modal-body'>
+                        <div className='doctor-info'>
+                            <DoctorProfile
+                                doctorId={this.props.doctorId}>
+                            </DoctorProfile>
+                        </div>
+                        <div className='row'>
+                            <div className='col-6 form-group'>
+                                <label>Name</label>
+                                <input className='form-control'></input>
+                            </div>
+                            <div className='col-6 form-group'>
+                                <label>Phone Number</label>
+                                <input className='form-control'></input>
+                            </div>
+                            <div className='col-6 form-group'>
+                                <label>Email</label>
+                                <input className='form-control'></input>
+                            </div>
+                            <div className='col-6 form-group'>
+                                <label>Address</label>
+                                <input className='form-control'></input>
+                            </div>
+                            <div className='col-6 form-group'>
+                                <label>Reason</label>
+                                <input className='form-control'></input>
+                            </div>
+                            <div className='col-6 form-group'>
+                                <label>Book For</label>
+                                <input className='form-control'></input>
+                            </div>
+                            <div className='col-6 form-group'>
+                                <label>Gender</label>
+                                <input className='form-control'></input>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='booking-modal-footer'>
+                        <button className='btn-booking-confirm'
+                            onClick={closeBookingModal}>
+                            Confirm
+                        </button>
+                        <button className='btn-booking-cancel'
+                            onClick={closeBookingModal}>
+                            Cancel
+                        </button>
+                    </div>
+                </div>
+            </Modal>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookingModal);
