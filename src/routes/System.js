76d1373ba3ae0f0847cb0fe404c6ef44a4ec5661
minhtriet2906 <swaggiehippdude@gmail.com>
2023-06-01 14:51:38 +0700

    create clinic, specialty routers

--- src/routes/System.js
+++ src/routes/System.js
@@ -5,6 +5,8 @@ import UserManage from '../containers/System/UserManage';
 import UserRedux from '../containers/System/Admin/UserRedux';
 import DoctorManage from '../containers/System/Admin/DoctorManage'
 import Header from '../containers/Header/Header';
+import SpecialtyManage from '../containers/System/Specialty/SpecialtyManage';
+import ClinicManage from '../containers/System/Clinic/ClinicManage';
 
 class System extends Component {
     render() {
@@ -19,7 +21,11 @@ class System extends Component {
                             <Route path="/system/user-manage" component={UserManage} />
                             <Route path="/system/user-redux" component={UserRedux} />
                             <Route path="/system/doctor-manage" component={DoctorManage} />
+                            <Route path="/system/specialty-manage" component={SpecialtyManage} />
+                            <Route path="/system/clinic-manage" component={ClinicManage} />
+
                             <Route component={() => { return (<Redirect to={systemMenuPath} />) }} />
+
                         </Switch>
                     </div>
                 </div>
