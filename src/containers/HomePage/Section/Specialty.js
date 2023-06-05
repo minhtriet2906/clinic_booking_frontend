068d05fb15b7cb2ec1080f1386b6717af042f15a
minhtriet2906 <swaggiehippdude@gmail.com>
2023-06-05 08:38:59 +0700

    update component specialty

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
+import { getAllSpecialtiesService } from '../../../services/userService';
+import * as actions from "../../../store/actions";
 
 import Slider from "react-slick";
 // Import css files
@@ -8,66 +10,58 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 class Specialty extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            specialties: [],
+        }
+    }
 
+    async componentDidMount() {
+        this.props.getAllSpecialties();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.specialties !== this.props.specialties) {
+            this.setState({
+                specialties: this.props.specialties,
+            })
+        }
+    }
 
     render() {
+        let specialtiesList = this.state.specialties;
+
         return (
             <div className="section-share section-specialty">
                 <div className="section-container">
                     <div className="section-header">
-                        <button>XEM THÊM</button>
-                        <span>Chuyên khoa phổ biến</span>
+                        <button><FormattedMessage id='homepage.more-info' /></button>
+                        <span><div><FormattedMessage id='homepage.specialty'></FormattedMessage></div></span>
                     </div>
                     <div className='section-body'>
                         <Slider {...this.props.settings}>
-                            <div className='section-customize'>
-                                <div className='outer-bg'>
-                                    <div className='bg-image img-specialty' />
-                                </div>
-                                <div className='position text-center'>
-                                    <div>Tim mạch 1</div>
-                                </div>
-                            </div>
-                            <div className='section-customize'>
-                                <div className='outer-bg'>
-                                    <div className='bg-image img-specialty' />
-                                </div>
-                                <div className='position text-center'>
-                                    <div>Tim mạch 2</div>
-                                </div>
-                            </div>
-                            <div className='section-customize'>
-                                <div className='outer-bg'>
-                                    <div className='bg-image img-specialty' />
-                                </div>
-                                <div className='position text-center'>
-                                    <div>Tim mạch 3</div>
-                                </div>
-                            </div>
-                            <div className='section-customize'>
-                                <div className='outer-bg'>
-                                    <div className='bg-image img-specialty' />
-                                </div>
-                                <div className='position text-center'>
-                                    <div>Tim mạch 4</div>
-                                </div>
-                            </div>
-                            <div className='section-customize'>
-                                <div className='outer-bg'>
-                                    <div className='bg-image img-specialty' />
-                                </div>
-                                <div className='position text-center'>
-                                    <div>Tim mạch5</div>
-                                </div>
-                            </div>
-                            <div className='section-customize'>
-                                <div className='outer-bg'>
-                                    <div className='bg-image img-specialty' />
-                                </div>
-                                <div className='position text-center'>
-                                    <div>Tim mạch 6</div>
-                                </div>
-                            </div>
+                            {specialtiesList && specialtiesList.length > 0 &&
+                                specialtiesList.map((item, index) => {
+                                    let imgBase64 = '';
+                                    if (item.image) {
+                                        imgBase64 = new Buffer(item.image, 'base64').toString('binary');
+                                    }
+                                    console.log(imgBase64);
+                                    return (
+                                        <div className='section-customize'>
+                                            <div className='outer-bg'>
+                                                <div className='bg-image img-specialty'
+                                                    style={{ backgroundImage: `url(${imgBase64})` }}
+                                                />
+                                            </div>
+                                            <div className='position text-center'>
+                                                <div>{item.name}</div>
+                                            </div>
+                                        </div>
+                                    )
+                                })
+                            }
                         </Slider>
                     </div>
                 </div>
@@ -81,11 +75,14 @@ const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
         language: state.app.language,
+        specialties: state.admin.specialties,
     };
 };
 
 const mapDispatchToProps = dispatch => {
     return {
+        getAllSpecialties: () => dispatch(actions.fetchAllSpecialties()),
+
     };
 };
 
