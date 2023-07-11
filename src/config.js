a818bec5272e2052e1d1591df489da3d8aef7f58
minhtriet2906 <swaggiehippdude@gmail.com>
2023-07-11 08:26:20 +0700

    refactor: update config api base url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
     api: {
-        API_BASE_URL: "http://localhost:8080/",
+        API_BASE_URL: process.env.REACT_APP_BACKEND_URL,
+        // "http://localhost:8080/",
         ROUTER_BASE_NAME: null,
     },
     app: {
