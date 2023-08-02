aab9393f3400d612ccff414ab021bb1cbc961a99
minhtriet2906 <swaggiehippdude@gmail.com>
2023-08-02 16:10:57 +0700

    feat: update date format

--- src/containers/System/Doctor/ScheduleManage.js
+++ src/containers/System/Doctor/ScheduleManage.js
@@ -147,12 +147,10 @@ class ScheduleManage extends Component {
         const year = date.getFullYear();
         const month = String(date.getMonth() + 1).padStart(2, '0');
         const day = String(date.getDate()).padStart(2, '0');
-        const hours = String(date.getHours()).padStart(2, '0');
-        const minutes = String(date.getMinutes()).padStart(2, '0');
-        const seconds = String(date.getSeconds()).padStart(2, '0');
+
 
         // Format the date in "yyyy-mm-dd HH:mm:ss" format
-        const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+        const formattedDate = `${year}-${month}-${day} ${0}:${0}:${0}`;
 
         if (timeSlotOptions && timeSlotOptions.length > 0) {
             let selectedTimeSlots = timeSlotOptions.filter(item => item.isSelected === true);
